ac0229395283ca2f628506ce3eb2df31abca0090
ClawBot <clawbot@nextasy.com>
2026-03-03 12:56:09 -0500

    feat: add CloudFront distribution for couplesapp-dev (free tier)

--- terraform/modules/s3-spa/outputs.tf
+++ terraform/modules/s3-spa/outputs.tf
@@ -13,3 +13,15 @@ output "website_endpoint" {
 output "website_url" {
   value = "http://${aws_s3_bucket_website_configuration.this.website_endpoint}"
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.this.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.this.id
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.this.domain_name}"
+}
