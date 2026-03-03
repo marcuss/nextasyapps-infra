7b9a6754691ec61d6ee2748d817695e4accc4431
Marcus Sanchez <marcuss@Marcuss-MacBook-Pro.local>
2026-03-02 23:07:22 -0500

    feat: add CloudFront distribution with HTTPS and SPA routing fix

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -5,3 +5,7 @@ output "couplesapp_frontend_url" {
 output "couplesapp_bucket_name" {
   value = module.couplesapp_frontend.bucket_name
 }
+
+output "couplesapp_cloudfront_url" {
+  value = module.couplesapp_frontend.cloudfront_url
+}
--- terraform/modules/s3-spa/outputs.tf
+++ terraform/modules/s3-spa/outputs.tf
@@ -9,3 +9,11 @@ output "bucket_arn" {
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.this.website_endpoint
 }
+
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.this.domain_name
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.this.domain_name}"
+}
